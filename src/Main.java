279020911392c0ae5e350a9c30437e272172c839
Gabo <chentevilo2001@gmail.com>
2021-04-22 19:52:12 -0600

    Primer Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static  void main (String[] args){
+        String instruccion ="";
+        System.out.println("\n______________________-----------------------------------______________________");
+        System.out.println("______________________--------- Orden Pacientes ------" +
+                "" +
+                "---______________________");
+        System.out.println("______________________-----------------------------------______________________");
+        System.out.println();
+    }
+}
